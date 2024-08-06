55290d0818ee8bbfca84b3bcb77f2225240c040f
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2024-08-06 20:37:34 +0530

    //

--- App.js
+++ App.js
@@ -16,4 +16,4 @@ const parent = React.createElement("div", { id: "parent" }, [
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   
-  root.render(parent);
+  root.render(parent);//
