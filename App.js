f708a49863351fc6fd3576664bed41d2628b710e
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2024-08-07 19:39:34 +0530

    ep3 part 1+2

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import ReactDOM from'react-dom/client';
+import ReactDOM from 'react-dom/client';
+
+//React Element
+
+const heading = React.createElement('h1',{id:"heading"},"Namaste React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
 
 
-
-
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "This is namaste react"),
-      React.createElement("h2", {}, "baby shark"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-  ]);
-  
-
-  
-  console.log(parent); 
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
