4cab0269402d3ad4fca83567caa0eaf2a0bd324f
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2024-08-07 14:20:14 +0530

    Episode2 Igniting the application

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import ReactDOM from'react-dom/client';
+
+
+
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
+      React.createElement("h1", {}, "This is namaste react"),
+      React.createElement("h2", {}, "baby shark"),
     ]),
     React.createElement("div", { id: "child2" }, [
       React.createElement("h1", {}, "I'm an h1 tag"),
@@ -16,4 +21,4 @@ const parent = React.createElement("div", { id: "parent" }, [
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   
-  root.render(parent);//
+  root.render(parent);
---
+++ README.md
@@ -0,0 +1,8 @@
+#Namaste React 
+
+#parcel
+-DEV build
+-Local server
+-Server refreshing automatically(HMR - HOT MODULE REPLACEMENT) -> FILE WATCHING ALOGORITHM written in c++
+-Image optimization aswell
+-when parcel will build production level project it will minify the bundle also 
