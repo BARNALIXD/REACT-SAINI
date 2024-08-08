ba1c69960d24705510ce7678d84f459f9d71b27f
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2024-08-08 18:42:57 +0530

    normal element and functional component

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-
+// normal element creation
 const heading = (
 <h1 className='heading'>
     Namaste reading using JSX
@@ -10,11 +10,28 @@ const heading = (
     );
 
 
-    // React Functional components
-
-    const Headingcomponent = () =>{
-        return <h1>Namaste react functional components</h1>
+//this is know as short hand rule for function
+    const fn = () => {
+        return true;
     }
+    const fn2 = () =>  true;
+
+
+
+
+    // React Functional components
+    const Headingcomponent = () => {
+        return <h1>Namaste react functional components</h1>
+    };
+    
+    // or this:
+    const HeadingComponent2 = () =>  (
+        <div id='container'>
+            <h1 className='heading'>Namaste react functional components</h1>
+        </div>
+    );
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
