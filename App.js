577cabf316e1495c5783a1011d2bc2194d03c47d
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2024-08-12 10:25:51 +0530

    props

--- App.js
+++ App.js
@@ -1,30 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Title = () => (
-<h1 className='heading' tabIndex={5}>
-    Namaste reading using JSX
-    </h1>
-    );
+/**
+ * Header
+ * -Logo
+ * -nav items
+ * Body
+ * -Search input
+ * -Card container
+ * -Restaurant card
+ * -img
+ * -name of the restaurant
+ * -name of the restaurant , cuisines, ratings , delivery time
+ * 
+ * Footer
+ * -adress
+ * -contact
+ * -links
+ * 
+ *  
+ */
+
+//Header
+const Header = () => {
+    return(
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo-image' src='https://i.pinimg.com/564x/41/ba/63/41ba6326746d7aed14737ce4334cad79.jpg' alt='Logo' />
+            </div>
+            <div className='nav-items'>
+             <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Cart</li>
+
+             </ul>
+            </div>
+        </div>
+    )
+};
 
 
 
-const HeadingComponent = () =>  (
-            <div id='container'>
 
-                //you can type title many number of times there is no limit 
 
-            <Title/>
-            <Title></Title>
-            {Title}
-            <h1 className='heading'>Namaste react functional components</h1>
+const styleCard = {
+    backgroundColor: "#121212 "
+};
+
+
+
+const RestaurantCard = (props) => {
+    console.log(props);
+    return( 
+        <div className='res-card' style={styleCard}>
+            <img  
+            className='res-logo'
+            alt='res-logo'   src='https://i.pinimg.com/564x/25/39/83/253983355089ece093804cbe5e0b22fa.jpg'></img>
+        <h3>{props.resName}</h3>
+        <h4>{props.cuisine}</h4>
+        <h4>{props.stars}</h4>
+        <h4>{props.time}</h4>
         </div>
     );
+};
+
+
+
+
+const Body = () => {
+    return(
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+             <RestaurantCard resName = "CHAI GPT" cuisine="drinks" stars = "4.5 stars" time = "40 mins"/>
+             <RestaurantCard resName = "KFC" cuisine = "chicken variez" stars = "5 stars" time = "30 mins" />
+
+            </div>
+        </div>
+    );
+};
+
+
+
+
+
+
+const AppLayout = () => {
+    return(
+        <div className='app'>
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
+
+
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<HeadingComponent/>); 
+root.render(<AppLayout/>); 
  
 
